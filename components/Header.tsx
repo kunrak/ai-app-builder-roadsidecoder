d8aad4571aaca4f70fbbf80ef5478280a3bc4f6c
kunrak <rakeshkunducsgi@gmail.com>
2026-08-01 06:22:22 +0530

    feat: integrate Prisma ORM, configure Postgres schema, and update header authentication UI components

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,14 +19,6 @@ function Header() {
         </Link>
 
         <div className="flex items-center gap-5">
-          <Link href={"/projects"} className="text-[13px] font-medium text-white/40 transition-colors hover:text-white/80">
-            Projects
-          </Link>
-
-          <button className="inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-xs text-white/70 ">
-            <Zap className="h-3 w-3 fill-white/70" />
-            3 / 40 Credits
-          </button>
 
           <div className="h-4 w-[1px] bg-white/10" />
 
@@ -44,6 +36,14 @@ function Header() {
           </Show>
 
           <Show when="signed-in">
+            <Link href={"/projects"} className="text-[13px] font-medium text-white/40 transition-colors hover:text-white/80">
+            Projects
+          </Link>
+
+          <span className="inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-xs text-white/70 ">
+            <Zap className="h-3 w-3 fill-white/70" />
+            3 / 40 Credits
+          </span>
             <UserButton />
           </Show>
         </div>
